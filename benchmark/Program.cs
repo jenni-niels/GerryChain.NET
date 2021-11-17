dc32ae4da729196e291330cddf1f805fdfd414c4
David Matthews <David.Matthews.1@uvm.edu>
2021-11-16 23:23:55 -0500

    fixing stopwatch issue where the total seconds was not printed and instead the integer precision seconds remainder when dividing by 60 was printed

--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -40,10 +40,10 @@ namespace benchmark
             var TallyPOP = Scores.TallyFactory("TOTPOP", "TOTPOP");
             var initPart = new Partition("../resources/al_vtds20_with_seeds.json", "CD_Seed", "TOTPOP", new string[] { "TOTPOP", "VAP", "BVAP" }, new Score[] { TallyBVAP, TallyPOP, TallyVAP });
             var chain = new ReComChain(initPart, stepCount, 0.01, batchSize:batchSize, degreeOfParallelism:degreeOfParallelism);
-            Console.WriteLine($"Loading data took: {sw.Elapsed.Seconds:F6} seconds");
+            Console.WriteLine($"Loading data took: {sw.Elapsed.TotalSeconds:F6} seconds");
             sw = Stopwatch.StartNew();
             var bs = chain.Select(p => ((DistrictWideScoreValue) p.Score("BVAP")).Value.Zip(((DistrictWideScoreValue)p.Score("VAP")).Value, (b, v) => b/v)).ToArray();
-            Console.WriteLine($"Chain took: {sw.Elapsed.Seconds:F6} seconds");
+            Console.WriteLine($"Chain took: {sw.Elapsed.TotalSeconds:F6} seconds");
 
         }
         public static void Main(string[] args) {
