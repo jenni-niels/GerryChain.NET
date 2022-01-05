81df5e9f912c76200b05abcd101f0b200ff29007
jenni-niels <jenniniels.matthews@gmail.com>
2022-01-04 20:05:15 -0500

    pcompress pin.

--- GerryChainExtensions/ExtensionScores/VRAEffectiveness.cs
+++ GerryChainExtensions/ExtensionScores/VRAEffectiveness.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using GerryChain;
 using Microsoft.FSharp.Collections;
 using VRA;
 
--- GerryChainExtensions/PCompress/Compressor.cs
+++ GerryChainExtensions/PCompress/Compressor.cs
@@ -1,29 +1,104 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+//using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+//using System.Linq;
+using System.Text;
 using GerryChain;
 
 namespace PCompress
 {
-    public class Record
+    public class Recorder
     {
         public Chain MarkovChain { get; init; }
+        public string FileName { get; init; }
+
+        public string Executable { get; init; }
+
+        public int Threads { get; init; }
+
+        /// <summary>
+        /// Returns a new Recorder class instance
+        /// </summary>
+        /// <param name="chain"> Instance of chain class to record. </param>
+        /// <param name="fileName"> File name to save to. </param>
+        /// <param name="executable"> Path to executable to run pcompressor </param>
+        /// <param name="threads"> How many threads to use in the recording. </param>
+        /// <remarks> TODO:: Do I want to use logical cores as default (Environment.ProcessorCount)
+        /// or someother system based measure of parallelizability? </remarks>
+        public Recorder(Chain chain, string fileName, string executable = "pcompress -e", int threads = 0)
+        {
+            MarkovChain = chain;
+            FileName = fileName;
+            Executable = executable;
+
+            if (threads > 0)
+            {
+                Threads = threads;
+            }
+            else
+            {
+                Threads = Environment.ProcessorCount;
+            }
+        }
+
+        public void Record()
+        {
+            var executingAssembly = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var executingAssemblyDir = Path.GetDirectoryName(executingAssembly);
+            
+            using (Process process = new Process())
+            {
+                var outputStream = new StreamWriter($"{ Environment.CurrentDirectory}/{FileName}");
+                process.StartInfo.FileName = "zsh";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+                process.StartInfo.Arguments = $"-c pcompress -e | xz -e -T {Threads}";
+                process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+                {
+                    if (!String.IsNullOrEmpty(e.Data))
+                    {
+                        outputStream.WriteLine(e.Data);
+                    }
+                });
+
+                process.Start();
+                process.BeginOutputReadLine();
+
+                StreamWriter writer = process.StandardInput;
+                
+                foreach (Partition p in MarkovChain)
+                {
+                    writer.WriteLine("[{0}]", string.Join(",", p.Assignments));
+                    //Console.WriteLine(p.SelfLoops);
+                }
+                writer.Close();
+
+                process.WaitForExit();
+                // Free resources associated with process.
+                process.Close();
+                outputStream.Close();
+            }
+        }
     }
 
-    public class Replay : IEnumerable<Partition>
-    {
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+    // public class Replayer : IEnumerable<Partition>
+    // {
+    //     IEnumerator IEnumerable.GetEnumerator()
+    //     {
+    //         return GetEnumerator();
+    //     }
 
-        public IEnumerator<Partition> GetEnumerator()
-        {
-            return new ChainReplayer(this);
-        }
+    //     public IEnumerator<Partition> GetEnumerator()
+    //     {
+    //         return new ChainReplayer(this);
+    //     }
 
-        public class ChainReplayer : IEnumerator<Partition>
-        {
-        }
-    }
+    //     public class ChainReplayer : IEnumerator<Partition>
+    //     {
+
+    //     }
+    // }
 }
